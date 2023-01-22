1ebf72ba2b0b797bb6d74fef04af393a75a585db
Meek <meek10x@gmail.com>
2023-01-21 23:08:35 -0600

    [chore] these will be main constants folder for  imports

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://api.devnet.solana.com";
+
+export const PROGRAM_ID = new PublicKey("5ExEq9ZxGfwSyw5ov4UFK7ws9NpDf2KnUk3wLqfAir4m")
