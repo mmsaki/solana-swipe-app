9f8d7e5311aab201e8ebf3132567ab6492abfe62
Meek <meek10x@gmail.com>
2023-01-24 17:35:38 -0600

    [feat] adding methods for buttons swipes

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,6 +1,5 @@
-import { CastRounded } from '@mui/icons-material';
 import React from 'react'
-import { useState } from 'react'
+import { useState, useMemo, useRef } from "react";
 import TinderCard from 'react-tinder-card'
 import "./TinderCards.css"
 
@@ -25,7 +24,7 @@ function TinderCards() {
       url: "https://media4.giphy.com/avatars/doodlesbyburnttoast/dMqxHmPPA8fd.gif",
     },
   ]);
-  
+
   const onSwipe = (direction) => {
     console.log("You swiped: " + direction);
   };
@@ -33,22 +32,98 @@ function TinderCards() {
   const onCardLeftScreen = (myIdentifier) => {
     console.log(myIdentifier + " left the screen");
   };
+  const [currentIndex, setCurrentIndex] = useState(people.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+  const currentIndexRef = useRef(currentIndex);
+  const childRefs = useMemo(
+    () =>
+      Array(people.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+  const canGoBack = currentIndex < people.length - 1;
+  const canSwipe = currentIndex >= 0;
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < people.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
 
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
 
   return (
-    <div className='card-container'>
-      {people.map((people) => (
-        <TinderCard
-          className='swipe'
-          key={people.name}
-          preventSwipe={['up', 'down']}
+    <div className='authed-container'>
+      <div className="cardContainer">
+        {people.map((character, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className="swipe"
+            key={character.name}
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
+          >
+            <div
+              style={{ backgroundImage: "url(" + character.url + ")" }}
+              className="card"
+            >
+              <h2>{character.name}</h2>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("left")}
         >
-
-            <div className="card" style={{ backgroundImage: `url(${people.url})` }}>
-            <h2>{people.name}</h2>
-          </div>
-        </TinderCard>
-      ))}
+          Swipe left!
+        </button>
+        <button
+          style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+          onClick={() => goBack()}
+        >
+          Undo swipe!
+        </button>
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("right")}
+        >
+          Swipe right!
+        </button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className="infoText">
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className="infoText">
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
     </div>
   );
 }
